c060dd70306e778a4931922f1e87cf78fbaf99e6
Arthur Debeaupte <arthurdebeaupte@mac.home>
2025-02-13 10:18:19 +0100

    init app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Happy Valentine's day my love !</h1>
+      <p>Count: {count}</p>
+      <button
+        className="custom-button"
+        onClick={() => setCount(count + 1)}>
+          Increment
+      </button>
     </div>
   );
 }
