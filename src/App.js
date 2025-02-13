8f29892c69840d4ed26e83054048d48bca0b338a
Arthur Debeaupte <arthurdebeaupte@mac.home>
2025-02-13 19:52:40 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import './App.css';
 import ResultCard from './components/ResultCard';
 
 const data = [
-  { option: 'GIFT n°1', style: { backgroundColor: '#F5AE99', textColor: 'white' }},
-  { option: 'GIFT n°2', style: { backgroundColor: '#F55D88', textColor: 'white' }},
-  { option: 'GIFT n°3', style: {backgroundColor: '#F54599', textColor: 'white'}},
-  { option: 'GIFT n°4', style: {backgroundColor: '#D95959', textColor: 'white'}},
-  { option: 'GIFT n°5', style: {backgroundColor: '#E60287', textColor: 'white'}}
+  { option: 'Gift n°1', style: { backgroundColor: '#F5AE99', textColor: 'white' }},
+  { option: 'Gift n°2', style: { backgroundColor: '#F55D88', textColor: 'white' }},
+  { option: 'Gift n°3', style: {backgroundColor: '#F54599', textColor: 'white'}},
+  { option: 'Gift n°4', style: {backgroundColor: '#D95959', textColor: 'white'}},
+  { option: 'Gift n°5', style: {backgroundColor: '#E60287', textColor: 'white'}}
 ];
 
 const prizeMessages = [
   "Awesome, you've won a fantastic 40L duffel bag.",
   "Amazing! We're gonna have the same bag !",
   "Lucky you! You've won a cool 36L duffel bag.",
-  "Fantastic! You've won the huge one ! 50L !.",
+  "Fantastic! You've won the huge one ! 50L !",
   "Wow! You've won an classy little bag."
 ];
 
@@ -62,7 +62,7 @@ const App = () => {
             console.log(prizeImages[prizeNumber]);
           }}
         />
-        <button className="custom-button" onClick={handleSpinClick}>SPIN</button>
+        <button className="custom-button" onClick={handleSpinClick}>Spin</button>
       </div>
       <div className="footer">
         <p className='ms-madi-regular'>Love u</p>
--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -4,10 +4,10 @@ import '../App.css';
 const ResultCard = ({ result, message, imageSrc, onClose }) => {
   return (
     <div className="result-card">
-      <h2>{result}</h2>
+      <h2 className='ms-madi-regular'>✨{result}✨</h2>
       <img src={imageSrc} alt={result} className="result-image" />
-      <p>{message}</p>
-      <button onClick={onClose}>Close</button>
+      <p className='ms-madi-regular'>{message}</p>
+      <button className='ms-madi-regular' onClick={onClose}>Close</button>
     </div>
   );
 };
